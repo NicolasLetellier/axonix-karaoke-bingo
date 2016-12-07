51fb3324962dee4a30fcb559e7863d3b7551bdb0
Nicolas Letellier <mrletelliernicolas@yahoo.fr>
2016-12-08 00:28:14 +0100

    add list.text and starting logic

--- js/karaoke.js
+++ js/karaoke.js
@@ -1,20 +1,36 @@
 $(document).ready(function(){
 
-  console.log("login.js on!");
-  
-  var loginButton = $("#login-button");
-
-  loginButton.on("click", clickLoginButton);
-
-  function clickLoginButton(event) {
-    event.preventDefault();
-    var email = $("#login-email-input").val();
-    var password = $("#login-password-input").val();
-    if (email === "axonix@test.com" && password === "test") {
-      window.location.href="campaign.html";
-    } else {
-      $("#failed-login-alert").slideDown();
-    }
+  function cleanFile(data) {
+    var lines = data.split('\n');
+    var mainFiltering = lines.filter(function(line){
+      return line !== 'SgList' && line.substring(0, 4) !== 'Page' ;
+    });
+    mainFiltering.splice(0, 1);
+    return mainFiltering;
   }
+
+  function searchList(event) {
+    event.preventDefault();
+
+  }
+
+  function bingo(event) {
+    event.preventDefault();
+    var list = event.data.list;
+    var randomIndex = Math.floor(Math.random() * list.length);
+    console.log(list[randomIndex]);
+    return list[randomIndex];
+  }
+
+  $.get('karaoke_list.txt', function(data) {
+
+    var list = cleanFile(data);
+
+    $('.search-button').on('click', { list: list, searchString: $('.search-input').val() }, searchList);
+
+    $('.bingo-button').on('click', { list: list }, bingo);
+
+  }, 'text');
+
 
 });
